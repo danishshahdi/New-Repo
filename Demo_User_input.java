c9cec8e510798906927c9fa7d8bc58dbe9168cc2
Md Danish Shahdi <danishshahdi111@gmail.com>
2022-02-01 14:25:24 +0530

    New Commit

---
+++ Demo_User_input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Demo_User_input{
+public static void main(String []args){
+	
+	
+	Scanner danish=new Scanner(System.in);
+	System.out.print("Enter Three number to find Greatest number:\n ");  
+    int a= danish.nextInt(); 
+	int b= danish.nextInt();
+	int c= danish.nextInt();
+	
+	System.out.print("You have Entered:" +a);  
+	System.out.print("\nYou have Entered:" +b);  
+	System.out.print("\nYou have Entered:" +c);  
+	
+	
+}
+	
+}
